9525c0c83e6600f8923975650107d7237983aa95
crurui <hanmengzz@126.com>
2018-02-20 08:11:16 +0800

    Update densecrf.pyx

--- pydensecrf/densecrf.pyx
+++ pydensecrf/densecrf.pyx
@@ -9,7 +9,9 @@ cimport eigen
 
 cdef ObjectiveFunction& _objectfunc(label, float robust):
     if isinstance(robust, Number):# && memoryview(label).ndim == 1:
-        return *(new LogLikelihood(eigen.c_vectorXf(label), robust))
+        LogLikelihood *pl = new LogLikelihood(eigen.c_vectorXf(label), robust)
+        LogLikelihood logl = *pl
+        return logl
     else:
         raise ValueError("Input Type Error")
     return 0
